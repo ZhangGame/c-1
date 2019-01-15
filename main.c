f0ffdfa7ba9a01d872ba1f566b7fafc8d3608052
unknown <627741487@qq.com>
2019-01-15 11:59:05 +0800

    第一次

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc,char*argv[])
+{
+    int qian=0;
+    printf("进入存钱系统\n ");
+    while(1){
+        int a=0;
+        printf("1--存钱\n");
+        printf("2--取钱\n");
+        printf("3--余额\n");
+        printf("4--退出\n");
 
-    printf("哈喽");
 
+        printf("请输入功能编号\n");
+        scanf("%d,&a");
+
+        if(a==1){
+            char x;
+            int cun;
+            printf("进入存钱功能，请输入要存入的金额\n");
+
+            scanf("%d,&cun");
+            qian=qian+cun;
+            printf("存入成功，点击回车继续\n");
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+    }
     return 0;
 
 }
