85f4048c4c64f3eecbbdd3e7e6b43ea4a1d24e6f
unknown <627741487@qq.com>
2019-01-15 15:59:44 +0800

    添加存钱功能

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
+
+    int qian =0;
+
     printf("进入钱包管理系统\n");
 
 
@@ -16,7 +19,15 @@ int main(){
         scanf("%d",&code);
 
         if(code == 1){
+            printf("请输入要存入的金额:\n");
+            int cun;
+            scanf("%d",&cun);
+            qian += cun;
 
+            printf("存入成功，点击回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
 
         if(code == 2){
